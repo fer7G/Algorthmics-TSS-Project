01447b30b351decf09e3aefa1a34e2c6b62243f4
GGaya <128990879+GGaya@users.noreply.github.com>
2023-04-09 19:51:40 +0200

    Marginal improvement + higher degree

--- greedyLT.cpp
+++ greedyLT.cpp
@@ -19,7 +19,7 @@ void printSet(Subset& S) {
     cout << S[n] << '}' << endl;
 }
 
-void readDegree(const Graph& G, priority_queue<pair<int,int> >& pq) {
+void readDegree(Graph& G, double r, priority_queue<pair<int,int> >& pq) {
     for (int i = 0; i < G.numNodes; ++i) {
             pair<int,int> p;
             p.first = G.adjList[i].size();
@@ -28,18 +28,25 @@ void readDegree(const Graph& G, priority_queue<pair<int,int> >& pq) {
         }
 }
 
+void readGain(Graph& G, double r, priority_queue<pair<int,int> >& pq) {
+    for (int i = 0; i < G.numNodes; ++i) {
+            Subset s(1, i);
+            int t = 0;
+            pq.push(make_pair(simulateLT(G, 0.5, s, t), i));
+        }
+}
+
 void findSubset(Subset& S, priority_queue<pair<int,int> >& pq, Graph& G, double r) {
-    while (!pq.empty()) {
-        int node = pq.top().second;
+    int v = pq.top().second;
+    pq.pop();
+    S.push_back(v);
+    int t = 0;
+    while (not pq.empty() and simulateLT(G, r, S, t) != G.numNodes) {
+        v = pq.top().second;
+        if (not G.influenced[v]) {
+            S.push_back(v);
+        }
         pq.pop();
-        S.push_back(node);
-        int t;
-
-        //Estas 2 variables porque por algun motivo 10 != 10
-        int aux1 = simulateLT(G, r, S,t);
-        int aux2 = G.numNodes;
-
-        if (aux1 == aux2) break;
     }
 }
 
@@ -48,12 +55,14 @@ int main () {
     Graph G= readGraph();
     double r = 0.5;
     priority_queue<pair<int,int> > pq; // first = grado, second = vertice
-    readDegree(G, pq);
+    //readDegree(G,r, pq);
+    readGain(G,r,pq);
     Subset S;
     auto start = high_resolution_clock::now();
     findSubset(S, pq, G, r);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
-    printSet(S);
-     cout << " en " << (double)duration.count()/1000 << " s";
+    //printSet(S);
+    cout << "Nodos del subset: "<< S.size()<< endl;
+    cout << "en " << (double)duration.count()/1000 << " s" << endl;
 }
